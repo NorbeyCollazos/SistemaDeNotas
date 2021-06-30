3d18c23f8e6b3328d176e36d59cd8ce7bf61c18f
Norbey <ncrcollazos@gmail.com>
2021-06-30 06:24:53 -0500

    se realiza la clase conexion y se comprueba

--- Usuarios/Modelo/Usuarios.php
+++ Usuarios/Modelo/Usuarios.php
@@ -1,4 +1,12 @@
 <?php
 
+require_once("././Conexion.php");
+
+class Usuarios extends Conexion{
+    public function __construct(){
+        $this->db = parent::__construct();
+    }
+}
+
 
 ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once('Usuarios/Modelo/Usuarios.php');
+
+$usuario = new Usuarios();
+
+?>
