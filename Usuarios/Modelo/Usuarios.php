bf40f28b13003c428fa32f08144fb46f145ba17f
Norbey <ncrcollazos@gmail.com>
2021-07-02 07:32:46 -0500

    se realiza los metodos de login y validaciones en la clase Usuarios

--- Usuarios/Modelo/Usuarios.php
+++ Usuarios/Modelo/Usuarios.php
@@ -1,12 +1,60 @@
 <?php
 
 require_once("././Conexion.php");
+session_start();
 
 class Usuarios extends Conexion{
 
     public function __construct()
     {
         $this->db = parent::__construct();
+    }
+
+    public function login($Usuario, $Pass)
+    {
+        $statement = $this->db->prepare("SELECT * FROM usuarios WHERE USUARIO = :Usuario AND PASS = :Pass");
+        $statement->bindParam(":Usuario",$Usuario);
+        $statement->bindParam(":Pass",$Pass);
+        $statement->execute();
+        if($statement->rowCount()== 1){
+            $result = $statement->fetch();
+            $_SESSION['NOMBRE'] = $result['NOMBRE']. " ".$result['APELLIDO'];
+            $_SESSION['ID'] = $result['ID_USUARIO'];
+            $_SESSION['PERFIL'] = $result['PERFIL'];
+            return true;
+        }
+        return false;
+    }
+
+    //metodos get
+    public function getNombre(){
+        return $_SESSION['NOMBRE'];
+    }
+
+    public function getId(){
+        return $_SESSION['ID'];
+    }
+
+    public function getPerfil(){
+        return $_SESSION['PERFIL'];
+    }
+
+    //validar session
+    public function validateSession(){
+        if($_SESSION["ID"] == null){
+            header('Location: ../../index.php');
+        }
+    }
+
+    //validadcion de perfil
+    public function validateSessionAdministrator(){
+        if($_SESSION["ID"] != null){
+            if($_SESSION["PERFIL"] == 'Docente'){
+                header('Location: ../../Estudiantes/Pages/index.php');
+            }else{
+                header('Location: ../../index.php');
+            }
+        }
     }
 
     public function add($Nombre, $Apellido, $Usuario, $Pass, $Perfil, $Estado)
